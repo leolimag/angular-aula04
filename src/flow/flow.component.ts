0dfd5d00d6b3209b66c1cbb6db844ed496608075
leolimag <83369558+leolimag@users.noreply.github.com>
2023-03-08 22:25:01 -0300

    created random flow to flow component

--- src/flow/flow.component.ts
+++ src/flow/flow.component.ts
@@ -1,19 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flow',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './flow.component.html',
   styleUrls: ['./flow.component.css'],
   standalone: true,
 })
 export class FlowComponent implements OnInit {
   constructor() {}
-  name: string = 'iris';
+  n: number = 10;
   ngOnInit() {}
 
   getNumbers() {
-    return [10, 3, 45, 0, 69, 12, 11];
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(Math.trunc(100 * Math.random()));
+    }
+    return arr;
   }
 }
